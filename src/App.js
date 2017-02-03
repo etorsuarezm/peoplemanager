2ab9a67560ef710851559bdf69baab9a872d0aa8
Hector Suarez <etorsuarez@gmail.com>
2017-02-03 10:30:32 -0600

    Add firebase module and import it in main component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
